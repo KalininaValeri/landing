8d77172da3ba365bf25427efa5d7ae5669d8ec46
KalininaValeri <kalinina.valeri@gmail.com>
2016-03-04 10:09:40 +0600

    slider wip

--- js/main.js
+++ js/main.js
@@ -2,15 +2,5 @@
  * Created by home on 02.03.2016.
  */
 $(function() {
-  $('#slider').cycle({
-
-            pagerAnchorBuilder: function(index, DOMelement) {
-                return '<a href="#" class="switch_element"></a>';
-            },
-         activePagerClass: 'active',
-          fx: 'shuffle',
-          speed: 100,
-    timeout: 10000,
-          pager: '#switch'
-});
+  $('.carousel').carousel()
 });
