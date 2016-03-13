7da63a86ed92a7bd2f3b11c443992ff90502297e
KalininaValeri <kalinina.valeri@gmail.com>
2016-03-13 11:24:55 +0600

    refactoring init map

--- js/main.js
+++ js/main.js
@@ -1,29 +1,22 @@
-/**
- * Created by home on 02.03.2016.
- */
-$(function() {
-  $('body').scrollspy({ target: '.header_navbar' });
+$(function () {
+    $('body').scrollspy({target: '.header_navbar'});
 
-
-  // $('.carousel').carousel();
+    initMap();
+    // $('.carousel').carousel();
 });
 
-function initialize() {
-//    var image = '/local/templates/main/img/marker.png';
-  var mapOptions = {
-    zoom: 17,
-    scrollwheel: false,
-    center: new google.maps.LatLng(55.014570881125,82.948096990585)
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+function initMap() {
+    var mapOptions = {
+        zoom: 17,
+        scrollwheel: false,
+        center: new google.maps.LatLng(55.014570881125, 82.948096990585)
+    };
 
-  var myLatLng164 = new google.maps.LatLng(55.014570881125,82.948096990585);
-  var Marker164 = new google.maps.Marker({
-    position: myLatLng164,
-    map: map,
-    // icon: image,
-    zoom: 17
-  });
-}
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    new google.maps.Marker({
+        position: new google.maps.LatLng(55.014570881125, 82.948096990585),
+        map: map,
+        zoom: 17
+    });
+};
